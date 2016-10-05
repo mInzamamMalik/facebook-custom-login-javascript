69c202628645550285b8a4bc8d1d38528f735bcf
malikasinger1 <malikasinger@gmail.com>
2016-10-05 19:23:36 +0500

    migrated to 'request'(an npm module) for making http request and removed nodejs native one

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require("path");
 var bodyparser = require("body-parser");
 var https = require('https');
 var cors = require('cors');
+var _request = require('request');
 
 var app = express();
 app.use(cors());
@@ -11,52 +12,34 @@ app.set('port', (process.env.PORT || 3000));
 app.set('APP_ID', "1286316678087115");
 app.set('APP_SECRET', "848300d200c6e5e3435a520ec2e0feef");
 
-
 //making http request is picked from here: http://stackoverflow.com/questions/9577611/http-get-request-in-node-js-express
-app.post("/verifyUserAccessToken", (request, response) => {
+app.post("/verifyUserAccessToken", (req, res) => {
 
-    var authObject = request.body.facebookAuthObject;
+    var authObject = req.body.facebookAuthObject;
     //token to be verified in this request
     var userToken = authObject.authResponse.accessToken;
     var userID = authObject.authResponse.userID;
 
-    //first getting App Access Token via graph API. Read more: https://developers.facebook.com/docs/facebook-login/access-tokens/  
-    var req = https.get({
-        host: 'graph.facebook.com',
-        path: '/oauth/access_token?client_id=' + app.get("APP_ID") + '&client_secret=' + app.get('APP_SECRET') + '&grant_type=client_credentials'
-    }, function (res) {
-        //console.log('STATUS: ' + res.statusCode);
-        //console.log('HEADERS: ' + JSON.stringify(res.headers));
+    _request('https://graph.facebook.com/oauth/access_token?client_id='
+        + app.get("APP_ID") + '&client_secret='
+        + app.get('APP_SECRET')
+        + '&grant_type=client_credentials',
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var APP_TOKEN = body.toString().split('=').pop();
 
-        var bodyChunks = [];
-        res.on('data', function (chunk) {
-            bodyChunks.push(chunk);
-        }).on('end', function (data) {
-            var body = Buffer.concat(bodyChunks);
-            console.log('BODY: ' + body);
-            var APP_TOKEN = body.toString().split('=').pop();
-            console.log('App Access Token: ' + APP_TOKEN);//now we have our facebook app token and we can verify user token using it so doing it in next lines
+                _request('https://graph.facebook.com/' + userID
+                    + '?fields=name,email,permissions,picture&input_token='
+                    + userToken + '&access_token=' + APP_TOKEN,
 
-
-            //making another http request
-            var req = https.get({
-                host: 'graph.facebook.com',
-                path: '/' + userID + '?fields=name,email,permissions,picture&input_token=' + userToken + '&access_token=' + APP_TOKEN
-            }, function (res) {
-                var bodyChunks = [];
-                res.on('data', function (chunk) {
-                    // You can process streamed parts here...
-                    bodyChunks.push(chunk);
-                }).on('end', function () {
-                    var body = Buffer.concat(bodyChunks);
-                    console.log('BODY: ' + body);
-                    var userObject = JSON.parse(body.toString());
-                    response.send(userObject);
-
-                })
-            });
+                    function (error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            console.log("body: ", body) // Show the HTML for the Google homepage.
+                            res.send(body);
+                        }
+                    })
+            }
         })
-    });
 });
 
 var indexPath = path.resolve(__dirname, "public");
